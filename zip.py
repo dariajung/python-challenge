5e86b388a6d7c0dbd2ac7729a727ff0e0ce8401f
Daria Jung <djj2115cu@gmail.com>
2014-11-27 12:13:08 -0500

    join all comments

--- zip.py
+++ zip.py
@@ -1,6 +1,6 @@
 import zipfile
 import os.path
-
+import re
 
 def unzip(source, destination):
     with zipfile.ZipFile(source) as zf:
@@ -10,7 +10,41 @@ def unzip(source, destination):
             path = destination
             zf.extract(member, path)
 
+def traverse_files(source):
+
+    match = ""
+    with open("channel/%s.txt" % source) as f:
+        data = f.read()
+        match = re.search("(Next nothing is )([0-9]+)", data).group(2)
+
+    nothings = [source, match]
+
+    while match:
+        with open("channel/%s.txt" % match) as f:
+            data = f.read()
+            search = re.search("(Next nothing is )([0-9]+)", data)
+            if search:
+                match = search.group(2)
+                nothings.append(match)
+            else:
+                match = None
+
+    return nothings
+
+
+def get_comments(source, nothings):
+    comments = []
+    with zipfile.ZipFile(source) as zf:
+        for f in nothings:
+            comments.append(zf.getinfo("%s.txt" % f).comment)
+
+    return "".join(comments)
+
 
 if __name__ == "__main__":
-    file_name = "channel.zip"
-    unzip(file_name, "channel/")
+    # file_name = "channel.zip"
+    # unzip(file_name, "channel/")
+
+    files = traverse_files("90052")
+
+    print get_comments("channel.zip", files)
